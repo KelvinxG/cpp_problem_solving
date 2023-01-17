356194ebac46ef3b2354a98a13618b5ff05ff78b
KelvinxG <42145481+KelvinxG@users.noreply.github.com>
2023-01-17 15:23:07 +0700

    Add files via upload

---
+++ fitness.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int i,t,x;
+	int y=10;
+	
+	cin>>t;
+	for(i=0;i<t;i++)
+	{
+	    cin>>x;
+	    int result;
+	    result=x*y;
+	    cout<<result<<endl;
+	}
+	
+	return 0;
+}
